11dac3845f7eefe2128d513dbdf5e9b486891d63
Lucas Santos <lucassantoos56@gmail.com>
2023-06-08 19:30:40 -0300

    added login logic.

---
+++ src/assets/js/login.js
@@ -0,0 +1,26 @@
+const buttonLogin = document.getElementById('buttonLogin');
+
+buttonLogin.addEventListener('click', () => {
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
+  const rememberMe = document.getElementById('rememberMe');
+  const errorPreview = document.getElementById('errorPreview');
+  const dataPreview = document.getElementById('dataPreview');
+
+  if (!email.value && !password.value) {
+    errorPreview.style.display = 'block';
+    return;
+  }
+
+  errorPreview.display = 'none';
+  dataPreview.style.display = 'block';
+  dataPreview.innerHTML = `<pre>{
+    email: ${email.value},
+    password: ${password.value},
+    rememberMe: ${rememberMe.checked}
+  }</pre>`;
+
+  setTimeout(() => {
+    window.location.href = '/login-journey/security.html';
+  }, 10000);
+});
