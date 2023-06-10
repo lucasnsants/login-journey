6fc69be1ef2af2aeb0d25c4c6d914938ac4f871e
Lucas Santos <lucassantoos56@gmail.com>
2023-06-10 17:52:42 -0300

    minor changes to the security logic.

--- src/assets/js/security.js
+++ src/assets/js/security.js
@@ -1,9 +1,6 @@
 let randomCodeSecurity = null;
 
 window.addEventListener('load', () => {
-  const passedErrorScreen = localStorage.getItem('passedErrorScreen');
-  if (passedErrorScreen === 'passed') localStorage.setItem('passedErrorScreen', 'notPassed');
-
   const randomNumber = document.getElementById('randomNumber');
   randomCodeSecurity = Math.floor(Math.random() * 100);
 
@@ -16,10 +13,10 @@ buttonSecurity.addEventListener('click', () => {
   const codeSecurity = document.getElementById('codeSecurity');
   const errorSecurity = document.getElementById('errorSecurity');
   const dataSecurity = document.getElementById('dataSecurity');
-
+  
   if (
     !codeSecurity.value ||
-    codeSecurity.value !== randomCodeSecurity
+    parseInt(codeSecurity.value) !== randomCodeSecurity
   ) {
     errorSecurity.style.display = 'block';
     return;
@@ -33,6 +30,11 @@ buttonSecurity.addEventListener('click', () => {
   }</pre>`;
 
   setTimeout(() => {
-    window.location.href = '/login-journey/generic_error.html';
-  }, 10000);
+    const passedErrorScreen = localStorage.getItem('passedErrorScreen');
+    if (passedErrorScreen === 'passed') {
+      window.location.href = '/login-journey/home.html';
+    } else {
+      window.location.href = '/login-journey/generic_error.html';
+    }
+  }, 5000);
 });
